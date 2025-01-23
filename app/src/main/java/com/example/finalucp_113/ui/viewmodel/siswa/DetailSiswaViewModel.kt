07b5fa9269909ee86ec088ac97cbf8fc71eae6ee
Rauda08 <raudajnnh08@gmail.com>
2025-01-23 23:43:17 +0700

    membuat view model untuk halaman siswa

---
+++ app/src/main/java/com/example/finalucp_113/ui/viewmodel/siswa/DetailSiswaViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.finalucp_113.ui.viewmodel.siswa
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalucp_113.model.Siswa
+import com.example.finalucp_113.repository.SiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class DetailsiswaUiState{
+    data class Success(val siswa: Siswa) : DetailsiswaUiState()
+    object Error : DetailsiswaUiState()
+    object Loading : DetailsiswaUiState()
+}
+
+class DetailsiswaViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val siswaRepository: SiswaRepository
+) : ViewModel() {
+
+    private val id_siswa: String = checkNotNull(savedStateHandle[DestinasiDetailsiswa.id_siswa])
+    var detailsiswaUiState: DetailsiswaUiState by mutableStateOf(DetailsiswaUiState.Loading)
+        private set
+
+    init {
+        getsiswabyId()
+    }
+
+    fun getsiswabyId(){
+        viewModelScope.launch {
+            detailsiswaUiState = DetailsiswaUiState.Loading
+            detailsiswaUiState = try {
+                DetailsiswaUiState.Success(siswa = siswaRepository.getSiswaByidSiswa(id_siswa))
+            } catch (e: IOException) {
+                DetailsiswaUiState.Error
+            }
+        }
+    }
+}
