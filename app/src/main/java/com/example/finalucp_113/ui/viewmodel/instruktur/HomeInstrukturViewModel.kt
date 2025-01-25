9734e61dd10932d1faab133c44de0743ddf58d18
Rauda08 <raudajnnh08@gmail.com>
2025-01-25 15:38:10 +0700

    membuat view model home instruktur

--- app/src/main/java/com/example/finalucp_113/ui/viewmodel/instruktur/HomeInstrukturViewModel.kt
+++ app/src/main/java/com/example/finalucp_113/ui/viewmodel/instruktur/HomeInstrukturViewModel.kt
@@ -1,2 +1,56 @@
 package com.example.finalucp_113.ui.viewmodel.instruktur
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalucp_113.model.Instruktur
+import com.example.finalucp_113.repository.InstrukturRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class InstrukturUiState {
+    data class Success(val instruktur: List<Instruktur>) : InstrukturUiState()
+    object Error : InstrukturUiState()
+    object Loading : InstrukturUiState()
+}
+
+class HomeInstrukturViewModel(private val instrukturRepository: InstrukturRepository) : ViewModel() {
+
+    var instrukturUiState: InstrukturUiState by mutableStateOf(InstrukturUiState.Success(emptyList())) // Inisialisasi dengan Success kosong
+        private set
+
+    init {
+        if ((instrukturUiState as? InstrukturUiState.Success)?.instruktur?.isEmpty() == true) {
+            getInstruktur()
+        }
+    }
+
+    fun getInstruktur() {
+        viewModelScope.launch {
+            instrukturUiState = InstrukturUiState.Loading
+            instrukturUiState = try {
+                val instrukturList = instrukturRepository.getInstruktur()
+                InstrukturUiState.Success(instrukturList)
+            } catch (e: IOException) {
+                InstrukturUiState.Error
+            } catch (e: HttpException) {
+                InstrukturUiState.Error
+            }
+        }
+    }
+
+    fun  deleteinstruktur ( id_instruktur :  String ) {
+        viewModelScope . launch  {
+            try  {
+                instrukturRepository. deleteInstruktur ( id_instruktur)
+            }  catch  (e : IOException){
+                InstrukturUiState . Error
+            }  catch  (e : HttpException){
+                InstrukturUiState . Error
+            }
+        }
+    }
+}
