fa73daa144f16bd73e27a23b7e49724e35b884b9
Rauda08 <raudajnnh08@gmail.com>
2025-01-25 16:44:39 +0700

    Menambahkan tampilan dan fungsi untuk mengelola data instruktur dihalaman home

--- app/src/main/java/com/example/finalucp_113/ui/view/instruktur/HomeInstrukturView.kt
+++ app/src/main/java/com/example/finalucp_113/ui/view/instruktur/HomeInstrukturView.kt
@@ -1,9 +1,5 @@
-package com.example.finalucp_113.ui.view.siswa
+package com.example.finalucp_113.ui.view.instruktur
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.TextButton
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,9 +14,22 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,88 +38,87 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.finalucp_113.model.Siswa
+import com.example.finalucp_113.model.Instruktur
 import com.example.finalucp_113.ui.customwidget.CostumeTopAppBar
 import com.example.finalucp_113.ui.navigation.DestinasiNavigasi
 import com.example.finalucp_113.ui.viewmodel.PenyediaViewModel
-import com.example.finalucp_113.ui.viewmodel.siswa.HomeSiswaViewModel
-import com.example.finalucp_113.ui.viewmodel.siswa.SiswaUiState
+import com.example.finalucp_113.ui.viewmodel.instruktur.HomeInstrukturViewModel
+import com.example.finalucp_113.ui.viewmodel.instruktur.InstrukturUiState
 
-
-object DestinasiHomeSiswa : DestinasiNavigasi {
-    override val route = "home_siswa"
-    override val titleRes = "Home Siswa"
+object DestinasiHomeInstruktur : DestinasiNavigasi {
+    override val route = "home_instruktur"
+    override val titleRes = "Home Instruktur"
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeSiswaView(
+fun HomeInstrukturView(
     modifier: Modifier = Modifier,
-    onAddSiswa: () -> Unit = {},
+    onAddInstruktur: () -> Unit = {},
     onDetailClick: (String) -> Unit = {},
-    viewModel: HomeSiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    viewModel: HomeInstrukturViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-                CostumeTopAppBar(
-                    title = "Home Data Siswa",
-                    scrollBehavior = scrollBehavior,
-                    canNavigateBack = false,
-                    onRefresh = {
-                        viewModel.getSiswa()
-                    }
-                )
+            CostumeTopAppBar(
+                title = "Home Data Instruktur",
+                scrollBehavior = scrollBehavior,
+                canNavigateBack = false,
+                onRefresh = {
+                    viewModel.getInstruktur()
+                }
+            )
         },
 
         floatingActionButton = {
             FloatingActionButton(
-                onClick = onAddSiswa,
+                onClick = onAddInstruktur,
                 containerColor = Color(0xFF46051C),
                 contentColor = Color.White,
                 modifier = Modifier.padding(16.dp)
             ) {
                 Icon(
                     imageVector = Icons.Default.Add,
-                    contentDescription = "Tambah Data Siswa"
+                    contentDescription = "Tambah Data Instruktur"
                 )
             }
         }
     ) { innerPadding ->
-        SiswaStatus(
-            siswaUiState = viewModel.siswaUIState,
-            retryAction = { viewModel.getSiswa() },
+        InstrukturStatus(
+            instrukturUiState = viewModel.instrukturUiState,
+            retryAction = { viewModel.getInstruktur() },
             modifier = Modifier.padding(innerPadding),
             onDetailClick = onDetailClick,
             onDeleteClick = {
-                viewModel.deletesiswa(it.id_siswa)
-                viewModel.getSiswa()
+                viewModel.deleteinstruktur(it.id_instruktur)
+                viewModel.getInstruktur()
             }
         )
     }
 }
 
 @Composable
-fun SiswaStatus(
-    siswaUiState: SiswaUiState,
+fun InstrukturStatus(
+    instrukturUiState: InstrukturUiState,
     modifier: Modifier = Modifier,
     retryAction :() -> Unit,
-    onDeleteClick: (Siswa) -> Unit = {},
+    onDeleteClick: (Instruktur) -> Unit = {},
     onDetailClick: (String) -> Unit
 ) {
-    when (siswaUiState) {
-        is SiswaUiState.Success -> {
-            if (siswaUiState.siswa.isEmpty()) {
+    when (instrukturUiState) {
+        is InstrukturUiState.Success -> {
+            if (instrukturUiState.instruktur.isEmpty()) {
                 Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text(text = "Tidak ada data Siswa")
+                    Text(text = "Tidak ada data Instruktur")
                 }
             } else {
-                SiswaLayout(
-                    siswa = siswaUiState.siswa,
+                InstrukturLayout(
+                    instruktur = instrukturUiState.instruktur,
                     modifier = modifier.fillMaxWidth(),
-                    onDetailClick = { onDetailClick(it.id_siswa.toString()) },
+                    onDetailClick = { onDetailClick(it.id_instruktur.toString()) },
                     onDeleteClick = { onDeleteClick(it) }
                 )
             }
@@ -120,25 +128,25 @@ fun SiswaStatus(
 }
 
 @Composable
-fun SiswaLayout(
-    siswa: List<Siswa>,
+fun InstrukturLayout(
+    instruktur: List<Instruktur>,
     modifier: Modifier = Modifier,
-    onDetailClick: (Siswa) -> Unit,
-    onDeleteClick: (Siswa) -> Unit = {}
+    onDetailClick: (Instruktur) -> Unit,
+    onDeleteClick: (Instruktur) -> Unit = {}
 ) {
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(siswa) { siswa ->
-            SiswaCard(
-                siswa = siswa,
+        items(instruktur) { instruktur ->
+            InstrukturCard(
+                instruktur = instruktur,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { onDetailClick(siswa) },
+                    .clickable { onDetailClick(instruktur) },
                 onDeleteClick = {
-                    onDeleteClick(siswa)
+                    onDeleteClick(instruktur)
                 }
             )
         }
@@ -146,10 +154,10 @@ fun SiswaLayout(
 }
 
 @Composable
-fun SiswaCard(
-    siswa: Siswa,
+fun InstrukturCard(
+    instruktur: Instruktur,
     modifier: Modifier = Modifier,
-    onDeleteClick: (Siswa) -> Unit = {}
+    onDeleteClick: (Instruktur) -> Unit = {}
 ) {
 
     var showDialog by remember { mutableStateOf(false) }
@@ -172,29 +180,17 @@ fun SiswaCard(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = siswa.nama_siswa,
+                    text = instruktur.nama_instruktur,
                     style = MaterialTheme.typography.titleMedium.copy(fontSize = 25.sp),
                     color = Color(0xFF46051C)
                 )
                 Text(
-                    text = siswa.email,
+                    text = instruktur.email,
                     style = MaterialTheme.typography.bodyMedium.copy(fontSize = 16.sp),
                     color = Color(0xFF46051C)
                 )
             }
 
-            Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                Text(
-                    text = "ID: ${siswa.id_siswa}",
-                    style = MaterialTheme.typography.bodyMedium.copy(fontSize = 16.sp),
-                    color = Color(0xFF46051C)
-                )
-                Text(
-                    text = "Telepon: ${siswa.no_telepon}",
-                    style = MaterialTheme.typography.bodyMedium.copy(fontSize = 16.sp),
-                    color = Color(0xFF46051C)
-                )
-            }
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -203,36 +199,11 @@ fun SiswaCard(
                 IconButton(onClick = { showDialog = true }) {
                     Icon(
                         imageVector = Icons.Default.Delete,
-                        contentDescription = "Hapus siswa",
+                        contentDescription = "Hapus Data Instruktur",
                         tint = Color(0xFF4D1414)
                     )
                 }
             }
         }
-    }
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text("Konfirmasi Hapus") },
-            text = { Text("Apakah Anda yakin ingin menghapus data siswa ini?") },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        onDeleteClick(siswa)
-                        showDialog = false
-                    }
-                ) {
-                    Text("Hapus")
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = { showDialog = false }
-                ) {
-                    Text("Batal")
-                }
-            },
-
-        )
     }
 }
