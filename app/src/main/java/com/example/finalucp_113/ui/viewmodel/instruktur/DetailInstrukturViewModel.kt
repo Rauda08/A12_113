e100e61cc021c3efbd1ded22a2073aa001efa6d0
Rauda08 <raudajnnh08@gmail.com>
2025-01-25 16:10:09 +0700

    Menambahkan ViewModel untuk detail instruktur dan penanganan state UI

--- app/src/main/java/com/example/finalucp_113/ui/viewmodel/instruktur/DetailInstrukturViewModel.kt
+++ app/src/main/java/com/example/finalucp_113/ui/viewmodel/instruktur/DetailInstrukturViewModel.kt
@@ -1,2 +1,52 @@
 package com.example.finalucp_113.ui.viewmodel.instruktur
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalucp_113.model.Instruktur
+import com.example.finalucp_113.repository.InstrukturRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class DetailInstrukturUiState{
+    data class Success(val instruktur: Instruktur) : DetailInstrukturUiState()
+    object Error : DetailInstrukturUiState()
+    object Loading : DetailInstrukturUiState()
+}
+
+class DetailInstrukturViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val instrukturRepository: InstrukturRepository
+) : ViewModel() {
+
+    private val id_instruktur: String = checkNotNull(savedStateHandle[DestinasiDetailInstruktur.id_instruktur])
+    var detailInstrukturUiState: DetailInstrukturUiState by mutableStateOf(DetailInstrukturUiState.Loading)
+        private set
+
+    init {
+        getinstrukturbyId()
+    }
+
+    fun getinstrukturbyId(){
+        viewModelScope.launch {
+            detailInstrukturUiState = DetailInstrukturUiState.Loading
+            detailInstrukturUiState = try {
+                DetailInstrukturUiState.Success(instruktur = instrukturRepository.getInstrukturByidInstruktur(id_instruktur))
+            } catch (e: IOException) {
+                DetailInstrukturUiState.Error
+            }
+        }
+    }
+}
+fun Instruktur.toDetailUiEvent () : InsertInstrukturEvent {
+    return InsertInstrukturEvent(
+        id_instruktur = id_instruktur,
+        nama_instruktur = nama_instruktur,
+        email = email,
+        no_telepon = no_telepon,
+        deskripsi = deskripsi
+    )
+}
