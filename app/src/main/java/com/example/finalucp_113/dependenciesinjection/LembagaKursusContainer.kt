51def2717bf2fbb927624a6b7a61323826f9915e
Rauda08 <raudajnnh08@gmail.com>
2025-01-25 16:31:18 +0700

    melengkapi kursus dan pendaftaran repository dengan lazy initialization di container

--- app/src/main/java/com/example/finalucp_113/dependenciesinjection/LembagaKursusContainer.kt
+++ app/src/main/java/com/example/finalucp_113/dependenciesinjection/LembagaKursusContainer.kt
@@ -1,7 +1,16 @@
 package com.example.finalucp_113.dependenciesinjection
 
+import com.example.finalucp_113.repository.InstrukturRepository
+import com.example.finalucp_113.repository.KursusRepository
+import com.example.finalucp_113.repository.NetworkInstrukturRepository
+import com.example.finalucp_113.repository.NetworkKursusRepository
+import com.example.finalucp_113.repository.NetworkPendaftaranRepository
 import com.example.finalucp_113.repository.NetworkSiswaRepository
+import com.example.finalucp_113.repository.PendaftaranRepository
 import com.example.finalucp_113.repository.SiswaRepository
+import com.example.finalucp_113.service.InstrukturService
+import com.example.finalucp_113.service.KursusService
+import com.example.finalucp_113.service.PendaftaranService
 import com.example.finalucp_113.service.SiswaService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -10,9 +19,9 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val siswaRepository: SiswaRepository
-//    val kursusRepository: KursusRepository
-//    val instrukturRepository: InstrukturRepository
-//    val pendaftaranRepository: PendaftaranRepository
+    val kursusRepository: KursusRepository
+    val instrukturRepository: InstrukturRepository
+    val pendaftaranRepository: PendaftaranRepository
 }
 
 class LembagaKursusContainer : AppContainer {
@@ -25,5 +34,13 @@ class LembagaKursusContainer : AppContainer {
     private val siswaService: SiswaService by lazy { retrofit.create(SiswaService::class.java) }
     override val siswaRepository: SiswaRepository by lazy { NetworkSiswaRepository(siswaService) }
 
+    private val instrukturService: InstrukturService by lazy { retrofit.create(InstrukturService::class.java) }
+    override val instrukturRepository: InstrukturRepository by lazy { NetworkInstrukturRepository(instrukturService) }
+
+    private val kursusService: KursusService by lazy { retrofit.create(KursusService::class.java) }
+    override val kursusRepository: KursusRepository by lazy { NetworkKursusRepository(kursusService) }
+
+    private val pendaftaranService: PendaftaranService by lazy { retrofit.create(PendaftaranService::class.java) }
+    override val pendaftaranRepository: PendaftaranRepository by lazy { NetworkPendaftaranRepository(pendaftaranService) }
 
 }
