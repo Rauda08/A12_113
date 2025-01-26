2dfe8ef2c575729d76b6ecec8fccdbc3a324f98e
Rauda08 <raudajnnh08@gmail.com>
2025-01-27 01:39:46 +0700

    menambah foto utnuk tampilan home menu dan menghias agar lebih menarik

--- app/src/main/java/com/example/finalucp_113/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/finalucp_113/ui/navigation/PengelolaHalaman.kt
@@ -1,6 +1,5 @@
 package com.example.finalucp_113.ui.navigation
 
-import DestinasiUpdateSiswa
 import UpdateSiswaView
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,38 +9,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.finalucp_113.ui.view.DestinasiHome
-import com.example.finalucp_113.ui.view.HomeMenuView
-import com.example.finalucp_113.ui.view.instruktur.DestinasiDetailInstruktur
-import com.example.finalucp_113.ui.view.instruktur.DestinasiHomeInstruktur
-import com.example.finalucp_113.ui.view.instruktur.DestinasiInsertInstruktur
-import com.example.finalucp_113.ui.view.instruktur.DestinasiUpdateInstruktur
-import com.example.finalucp_113.ui.view.instruktur.DetailInstrukturView
-import com.example.finalucp_113.ui.view.instruktur.HomeInstrukturView
-import com.example.finalucp_113.ui.view.instruktur.InsertInstrukturView
-import com.example.finalucp_113.ui.view.instruktur.UpdateInstrukturView
-import com.example.finalucp_113.ui.view.kursus.DestinasiDetailKursus
-import com.example.finalucp_113.ui.view.kursus.DestinasiHomeKursus
-import com.example.finalucp_113.ui.view.kursus.DestinasiInsertKursus
-import com.example.finalucp_113.ui.view.kursus.DestinasiUpdateKursus
-import com.example.finalucp_113.ui.view.kursus.DetailkursusView
-import com.example.finalucp_113.ui.view.kursus.HomeKursusView
-import com.example.finalucp_113.ui.view.kursus.InsertKursusView
-import com.example.finalucp_113.ui.view.kursus.UpdateKursusView
-import com.example.finalucp_113.ui.view.pendaftaran.DestinasiDetailPendaftaran
-import com.example.finalucp_113.ui.view.pendaftaran.DestinasiHomePendaftaran
-import com.example.finalucp_113.ui.view.pendaftaran.DestinasiInsertPendaftaran
-import com.example.finalucp_113.ui.view.pendaftaran.DestinasiUpdatePendaftaran
-import com.example.finalucp_113.ui.view.pendaftaran.DetailPendaftaranView
-import com.example.finalucp_113.ui.view.pendaftaran.HomePendaftaranView
-import com.example.finalucp_113.ui.view.pendaftaran.InsertPendaftaranView
-import com.example.finalucp_113.ui.view.pendaftaran.UpdatePendaftaranView
-import com.example.finalucp_113.ui.view.siswa.DestinasiDetailSiswa
-import com.example.finalucp_113.ui.view.siswa.DestinasiHomeSiswa
-import com.example.finalucp_113.ui.view.siswa.DestinasiInsertSiswa
-import com.example.finalucp_113.ui.view.siswa.DetailSiswaView
-import com.example.finalucp_113.ui.view.siswa.HomeSiswaView
-import com.example.finalucp_113.ui.view.siswa.InsertSiswaView
+import com.example.finalucp_113.ui.view.*
+import com.example.finalucp_113.ui.view.instruktur.*
+import com.example.finalucp_113.ui.view.kursus.*
+import com.example.finalucp_113.ui.view.pendaftaran.*
+import com.example.finalucp_113.ui.view.siswa.*
 
 @Composable
 fun PengelolaHalaman(
@@ -53,314 +25,204 @@ fun PengelolaHalaman(
         startDestination = DestinasiHome.route,
         modifier = Modifier
     ) {
-        composable(
-            route = DestinasiHome.route
-        ) {
+        /////////////Halaman Home////////////////
+
+        composable(route = DestinasiHome.route) {
             HomeMenuView(
                 onSiswaClick = { navController.navigate(DestinasiHomeSiswa.route) },
-                onInstrukturClick = { navController.navigate(DestinasiHomeInstruktur.route)},
-                onKursusClick = { navController.navigate(DestinasiHomeKursus.route)},
-                onPendaftaranClick = { navController.navigate(DestinasiHomePendaftaran.route)}
+                onInstrukturClick = { navController.navigate(DestinasiHomeInstruktur.route) },
+                onKursusClick = { navController.navigate(DestinasiHomeKursus.route) },
+                onPendaftaranClick = { navController.navigate(DestinasiHomePendaftaran.route) }
             )
         }
-        composable(
-            route = DestinasiHomeSiswa.route
-        ) {
+
+        /////////////Navigasi Siswa/////////////
+
+        composable(route = DestinasiHomeSiswa.route) {
             HomeSiswaView(
-                onAddSiswa = {
-                    navController.navigate(DestinasiInsertSiswa.route)
-                },
+                onAddSiswa = { navController.navigate(DestinasiInsertSiswa.route) },
                 onDetailClick = { id_siswa ->
                     navController.navigate("${DestinasiDetailSiswa.route}/$id_siswa")
                 },
                 modifier = modifier
             )
         }
-
-        composable(
-            route = DestinasiInsertSiswa.route
-        ) {
+        composable(route = DestinasiInsertSiswa.route) {
             InsertSiswaView(
-                onBack = {
-                    navController.popBackStack()
-                },
-                onNavigate = {
-                    navController.popBackStack()
-                },
-                modifier = Modifier
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
             )
         }
-
         composable(
             route = DestinasiDetailSiswa.routeWithArgs,
             arguments = listOf(
-                navArgument(DestinasiDetailSiswa.id_siswa) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiDetailSiswa.id_siswa) { type = NavType.StringType }
             )
         ) {
             val id_siswa = it.arguments?.getString(DestinasiDetailSiswa.id_siswa)
-
-            id_siswa?.let { id_siswa ->
+            id_siswa?.let { id ->
                 DetailSiswaView(
-                    navigateBack = {
-                        navController.popBackStack()
-                    },
-                    onEditClick = { id_siswa ->
-                        navController.navigate("${DestinasiUpdateSiswa.route}/$id_siswa")
-                    }
+                    navigateBack = { navController.popBackStack() },
+                    onEditClick = { navController.navigate("${DestinasiUpdateSiswa.route}/$id") }
                 )
             }
         }
-
         composable(
-            DestinasiUpdateSiswa.routeWithArgs,
+            route = DestinasiUpdateSiswa.routeWithArgs,
             arguments = listOf(
-                navArgument (DestinasiUpdateSiswa.id_siswa) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiUpdateSiswa.id_siswa) { type = NavType.StringType }
             )
         ) {
             UpdateSiswaView(
-                navigateBack = {
-                    navController.popBackStack()
-                },
+                navigateBack = { navController.popBackStack() },
                 onNavigateUp = {
-                    navController.navigate(
-                        DestinasiUpdateSiswa.route
-                    ){
-                        popUpTo(DestinasiHome.route){
-                            inclusive = true
-                        }
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
                     }
-                },
+                }
             )
         }
 
-        //Instruktur
 
-        composable(
-            route = DestinasiHomeInstruktur.route
-        ) {
+        ///////// Navigasi Instruktur//////////////
+
+
+        composable(route = DestinasiHomeInstruktur.route) {
             HomeInstrukturView(
-                onAddInstruktur = {
-                    navController.navigate(DestinasiInsertInstruktur.route)
-                },
+                onAddInstruktur = { navController.navigate(DestinasiInsertInstruktur.route) },
                 onDetailClick = { id_instruktur ->
                     navController.navigate("${DestinasiDetailInstruktur.route}/$id_instruktur")
                 },
                 modifier = modifier
             )
         }
-
-        composable(
-            route = DestinasiInsertInstruktur.route
-        ) {
+        composable(route = DestinasiInsertInstruktur.route) {
             InsertInstrukturView(
-                onBack = {
-                    navController.popBackStack()
-                },
-                onNavigate = {
-                    navController.popBackStack()
-                },
-                modifier = Modifier
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
             )
         }
-
         composable(
             route = DestinasiDetailInstruktur.routeWithArgs,
             arguments = listOf(
-                navArgument(DestinasiDetailInstruktur.id_instruktur) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiDetailInstruktur.id_instruktur) { type = NavType.StringType }
             )
         ) {
             val id_instruktur = it.arguments?.getString(DestinasiDetailInstruktur.id_instruktur)
-
-            id_instruktur?.let { id_instruktur ->
+            id_instruktur?.let { id ->
                 DetailInstrukturView(
-                    navigateBack = {
-                        navController.popBackStack()
-                    },
-                    onEditClick = { id_instruktur ->
-                        navController.navigate("${DestinasiUpdateInstruktur.route}/$id_instruktur")
-                    }
+                    navigateBack = { navController.popBackStack() },
+                    onEditClick = { navController.navigate("${DestinasiUpdateInstruktur.route}/$id") }
                 )
             }
         }
-
         composable(
-            DestinasiUpdateInstruktur.routeWithArgs,
+            route = DestinasiUpdateInstruktur.routeWithArgs,
             arguments = listOf(
-                navArgument (DestinasiUpdateInstruktur.id_instruktur) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiUpdateInstruktur.id_instruktur) { type = NavType.StringType }
             )
         ) {
             UpdateInstrukturView(
-                navigateBack = {
-                    navController.popBackStack()
-                },
+                navigateBack = { navController.popBackStack() },
                 onNavigateUp = {
-                    navController.navigate(
-                        DestinasiUpdateInstruktur.route
-                    ){
-                        popUpTo(DestinasiHome.route){
-                            inclusive = true
-                        }
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
                     }
-                },
+                }
             )
         }
 
-        // Kursus
+        /////////// Navigasi Kursus////////////
 
-        composable(
-            route = DestinasiHomeKursus.route
-        ) {
+        composable(route = DestinasiHomeKursus.route) {
             HomeKursusView(
-                onAddKursus = {
-                    navController.navigate(DestinasiInsertKursus.route)
-                },
+                onAddKursus = { navController.navigate(DestinasiInsertKursus.route) },
                 onDetailClick = { id_kursus ->
                     navController.navigate("${DestinasiDetailKursus.route}/$id_kursus")
                 },
                 modifier = modifier
             )
         }
-
-        composable(
-            route = DestinasiInsertKursus.route
-        ) {
+        composable(route = DestinasiInsertKursus.route) {
             InsertKursusView(
-                onBack = {
-                    navController.popBackStack()
-                },
-                onNavigate = {
-                    navController.popBackStack()
-                },
-                modifier = Modifier
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
             )
         }
-
         composable(
             route = DestinasiDetailKursus.routeWithArgs,
             arguments = listOf(
-                navArgument(DestinasiDetailKursus.id_kursus) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiDetailKursus.id_kursus) { type = NavType.StringType }
             )
         ) {
             val id_kursus = it.arguments?.getString(DestinasiDetailKursus.id_kursus)
-
-            id_kursus?.let { id_kursus ->
+            id_kursus?.let { id ->
                 DetailkursusView(
-                    navigateBack = {
-                        navController.popBackStack()
-                    },
-                    onEditClick = { id_kursus ->
-                        navController.navigate("${DestinasiUpdateKursus.route}/$id_kursus")
-                    }
+                    navigateBack = { navController.popBackStack() },
+                    onEditClick = { navController.navigate("${DestinasiUpdateKursus.route}/$id") }
                 )
             }
         }
-
         composable(
-            DestinasiUpdateKursus.routeWithArgs,
+            route = DestinasiUpdateKursus.routeWithArgs,
             arguments = listOf(
-                navArgument (DestinasiUpdateKursus.id_kursus) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiUpdateKursus.id_kursus) { type = NavType.StringType }
             )
         ) {
             UpdateKursusView(
-                navigateBack = {
-                    navController.popBackStack()
-                },
+                navigateBack = { navController.popBackStack() },
                 onNavigateUp = {
-                    navController.navigate(
-                        DestinasiUpdateKursus.route
-                    ){
-                        popUpTo(DestinasiHome.route){
-                            inclusive = true
-                        }
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
                     }
-                },
+                }
             )
         }
 
-        ///////Pendaftaran/////////////
+        ////////// Navigasi Pendaftaran//////////////
 
-        composable(
-            route = DestinasiHomePendaftaran.route
-        ) {
+        composable(route = DestinasiHomePendaftaran.route) {
             HomePendaftaranView(
-                onAddPendaftaran = {
-                    navController.navigate(DestinasiInsertPendaftaran.route)
-                },
+                onAddPendaftaran = { navController.navigate(DestinasiInsertPendaftaran.route) },
                 onDetailClick = { id_pendaftaran ->
                     navController.navigate("${DestinasiDetailPendaftaran.route}/$id_pendaftaran")
                 },
                 modifier = modifier
             )
         }
-
-        composable(
-            route = DestinasiInsertPendaftaran.route
-        ) {
+        composable(route = DestinasiInsertPendaftaran.route) {
             InsertPendaftaranView(
-                onBack = {
-                    navController.popBackStack()
-                },
-                onNavigate = {
-                    navController.popBackStack()
-                },
-                modifier = Modifier
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
             )
         }
-
         composable(
             route = DestinasiDetailPendaftaran.routeWithArgs,
             arguments = listOf(
-                navArgument(DestinasiDetailPendaftaran.id_pendaftaran) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiDetailPendaftaran.id_pendaftaran) { type = NavType.StringType }
             )
         ) {
             val id_pendaftaran = it.arguments?.getString(DestinasiDetailPendaftaran.id_pendaftaran)
-
-            id_pendaftaran?.let { id_pendaftaran ->
+            id_pendaftaran?.let { id ->
                 DetailPendaftaranView(
-                    navigateBack = {
-                        navController.popBackStack()
-                    },
-                    onEditClick = { id_pendaftaran ->
-                        navController.navigate("${DestinasiUpdatePendaftaran.route}/$id_pendaftaran")
-                    }
+                    navigateBack = { navController.popBackStack() },
+                    onEditClick = { navController.navigate("${DestinasiUpdatePendaftaran.route}/$id") }
                 )
             }
         }
         composable(
-            DestinasiUpdateKursus.routeWithArgs,
+            route = DestinasiUpdatePendaftaran.routeWithArgs,
             arguments = listOf(
-                navArgument (DestinasiUpdateKursus.id_kursus) {
-                    type = NavType.StringType
-                }
+                navArgument(DestinasiUpdatePendaftaran.id_pendaftaran) { type = NavType.StringType }
             )
         ) {
             UpdatePendaftaranView(
-                navigateBack = {
-                    navController.popBackStack()
-                },
+                navigateBack = { navController.popBackStack() },
                 onNavigateUp = {
-                    navController.navigate(
-                        DestinasiUpdatePendaftaran.route
-                    ){
-                        popUpTo(DestinasiHome.route){
-                            inclusive = true
-                        }
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
                     }
-                },
+                }
             )
         }
     }
--- app/src/main/java/com/example/finalucp_113/ui/view/HomeMenu.kt
+++ app/src/main/java/com/example/finalucp_113/ui/view/HomeMenu.kt
@@ -1,21 +1,30 @@
 package com.example.finalucp_113.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.finalucp_113.R
 import com.example.finalucp_113.ui.navigation.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi {
@@ -23,46 +32,59 @@ object DestinasiHome : DestinasiNavigasi {
     override val titleRes = "Dashboard"
 }
 
-
 @Composable
 fun HomeMenuView(
     onSiswaClick: () -> Unit,
-    onInstrukturClick : () -> Unit,
-    onKursusClick : () -> Unit,
-    onPendaftaranClick : () -> Unit
+    onInstrukturClick: () -> Unit,
+    onKursusClick: () -> Unit,
+    onPendaftaranClick: () -> Unit
 ) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)
-            .background(Color(0xFDEAABEF))
-        ,
-        verticalArrangement = Arrangement.Top,
+            .padding(0.dp)
+            .background(Color(0xFDF3A2BE)),
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.kursuss),
+            contentDescription = "Logo Lembaga Kursus",
+            modifier = Modifier
+                .fillMaxWidth(0.4f)
+                .padding(top = 30.dp)
+        )
+
         Text(
             text = "Selamat Datang",
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold,
-            color = Color(0xFFB75C8E), // Deep pink color
-            modifier = Modifier.padding(top = 40.dp)
+            color = Color(0xFF46051C),
+            modifier = Modifier.padding(top = 20.dp)
         )
 
         Text(
-            text = "di aplikasi Lembaga Kursus ",
+            text = "di aplikasi Lembaga Kursus",
             fontSize = 18.sp,
             fontWeight = FontWeight.Bold,
-            color = Color(0xFFB75C8E), // Deep pink color
-            modifier = Modifier.padding(top = 16.dp)
+            color = Color(0xFF46051C),
+            modifier = Modifier.padding(top = 10.dp)
         )
 
         Button(
             onClick = onSiswaClick,
             modifier = Modifier
-                .padding(50.dp)
+                .padding(20.dp)
                 .fillMaxWidth(0.8f),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD05E99)) // Light pink for button
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF46051C))
         ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Menu Siswa",
+                modifier = Modifier.padding(end = 10.dp),
+                tint = Color.White
+            )
             Text(
                 text = "Menu Siswa",
                 fontSize = 20.sp,
@@ -74,10 +96,16 @@ fun HomeMenuView(
         Button(
             onClick = onInstrukturClick,
             modifier = Modifier
-                .padding(50.dp)
+                .padding(20.dp)
                 .fillMaxWidth(0.8f),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD05E99)) // Light pink for button
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF46051C))
         ) {
+            Icon(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "Menu Instruktur",
+                modifier = Modifier.padding(end = 10.dp),
+                tint = Color.White
+            )
             Text(
                 text = "Menu Instruktur",
                 fontSize = 20.sp,
@@ -85,13 +113,20 @@ fun HomeMenuView(
                 color = Color.White
             )
         }
+
         Button(
             onClick = onKursusClick,
             modifier = Modifier
-                .padding(50.dp)
+                .padding(20.dp)
                 .fillMaxWidth(0.8f),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD05E99))
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF46051C))
         ) {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = "Menu Kursus",
+                modifier = Modifier.padding(end = 10.dp),
+                tint = Color.White
+            )
             Text(
                 text = "Menu Kursus",
                 fontSize = 20.sp,
@@ -103,10 +138,16 @@ fun HomeMenuView(
         Button(
             onClick = onPendaftaranClick,
             modifier = Modifier
-                .padding(50.dp)
+                .padding(20.dp)
                 .fillMaxWidth(0.8f),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD05E99))
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF46051C))
         ) {
+            Icon(
+                imageVector = Icons.Default.Create,
+                contentDescription = "Menu Pendaftaran",
+                modifier = Modifier.padding(end = 10.dp),
+                tint = Color.White
+            )
             Text(
                 text = "Menu Pendaftaran",
                 fontSize = 20.sp,
@@ -114,6 +155,5 @@ fun HomeMenuView(
                 color = Color.White
             )
         }
-
-        }
+    }
 }
